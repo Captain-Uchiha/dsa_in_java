7169e99068af5ffbf31b39415f7798a337e7df18
captain_uchiha <pvikash075@gmail.com>
2023-08-27 18:05:18 +0530

    MoveZeroesToEnd RemoveDuplicatesFromSortedArray SecondLargestElementInArray

---
+++ src/main/java/array/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,39 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class RemoveDuplicatesFromSortedArray {
+
+    // return count of unique elements in a sorted array
+
+    public static int countOfUniqueElements(int[] arr)
+    {
+        int res = 1;
+        for(int i =1;i<arr.length;i++)
+        {
+            if(arr[i]!=arr[res-1])
+            {
+                arr[res] = arr[i];
+                res++;
+            }
+        }
+        return res;
+    }
+
+
+
+    public static void main(String[] args)
+    {
+
+        int[] testCase_1 = new int[]{10,10,20,40};
+        int[] testCase_2 = new int[]{22,22,22,22};
+        int[] testCase_3 = new int[]{10};
+
+        System.out.println(countOfUniqueElements(testCase_1));
+        System.out.println(countOfUniqueElements(testCase_2));
+        System.out.println(countOfUniqueElements(testCase_3));
+
+    }
+}
---
+++ src/main/java/array/SecondLargestElementInArray.java
@@ -0,0 +1,45 @@
+package array;
+
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class SecondLargestElementInArray {
+
+    public static int findSecondLargest(int[] arr) {
+        if (arr.length <= 1) {
+            return -1; // Not enough elements to find the second-largest
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            return -1; // No second-largest element found
+        }
+        return secondLargest;
+    }
+
+
+
+    public static void main(String[] args)
+    {
+
+        int[] testCase_1 = new int[]{10,5,8,20};
+        int[] testCase_2 = new int[]{20,10,20,8,12};
+        int[] testCase_3 = new int[]{10,10,10};
+
+        System.out.println(findSecondLargest(testCase_1));
+        System.out.println(findSecondLargest(testCase_2));
+        System.out.println(findSecondLargest(testCase_3));
+    }
+}
