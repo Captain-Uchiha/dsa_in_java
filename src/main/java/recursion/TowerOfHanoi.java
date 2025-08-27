2c2d48de58e76dd0f3121c913758d82e8854d23a
captain_uchiha <pvikash075@gmail.com>
2025-08-27 22:13:36 +0530

    tower of hanoi with tree

--- src/main/java/recursion/TowerOfHanoi.java
+++ src/main/java/recursion/TowerOfHanoi.java
@@ -3,14 +3,15 @@ package recursion;
 public class TowerOfHanoi {
 
 
-    static void solution(int n, char a, char c, char b){
+    static void solution(int n, char a, char b, char c){
 
         if(n==0)
             return;
 
-        solution(n-1,a,b,c);
-        System.out.println(a +"->" + c);
-        solution(n-1,b,c,a);
+        solution(n-1,a,c,b);
+        System.out.println(a + "->"+ c);
+        solution(n-1,b,a,c);
+
 
     }
 
@@ -18,3 +19,40 @@ public class TowerOfHanoi {
         solution(3,'a','b','c');
     }
 }
+/**
+solution(3, a, b, c)
+│
+        ├── solution(2, a, c, b)
+│   │
+        │   ├── solution(1, a, b, c)
+│   │   │
+        │   │   ├── solution(0, a, c, b)  // returns
+│   │   ├── print a->c
+│   │   └── solution(0, b, a, c)  // returns
+│   │
+        │   ├── print a->b
+│   │
+        │   └── solution(1, c, a, b)
+│       │
+        │       ├── solution(0, c, b, a)  // returns
+│       ├── print c->b
+│       └── solution(0, a, c, b)  // returns
+│
+        ├── print a->c
+│
+        └── solution(2, b, a, c)
+    │
+            ├── solution(1, b, c, a)
+    │   │
+            │   ├── solution(0, b, a, c)  // returns
+    │   ├── print b->a
+    │   └── solution(0, c, b, a)  // returns
+    │
+            ├── print b->c
+    │
+            └── solution(1, a, b, c)
+        │
+                ├── solution(0, a, c, b)  // returns
+        ├── print a->c
+        └── solution(0, b, a, c)  // returns
+ **/
